29c5f297ca8a44985a08218acd229c5abc131101
Prihodko Nick <prihodkonn@gmail.com>
2020-05-17 20:30:24 +0300

    add js-practice tasks

---
+++ js-practice/flat-array/index.js
@@ -0,0 +1,16 @@
+let start = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
+let res = [];
+
+function flat(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flat(arr[i]);
+    } else {
+      res.push(arr[i]);
+    }
+  }
+}
+
+flat(start);
+
+console.log(res);
